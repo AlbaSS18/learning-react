b1a675bbba4b28137bb7376da3cc2d29df2ca447
albass18 <albaserenasuarez1998@gmail.com>
2024-09-29 22:54:33 +0200

    format code

--- projects/01-tic-tac-toe/src/logic/board.js
+++ projects/01-tic-tac-toe/src/logic/board.js
@@ -1,23 +1,22 @@
 
 import { WINNER_COMBOS } from "../constants";
+
 export const checkWinner = (boardToCheck) => {
-    // revisamos todas las combinaciones ganadoras
-    for(const combo of WINNER_COMBOS){
-      const [a, b, c] = combo;
-      if(
-        boardToCheck[a] &&
-        boardToCheck[a] === boardToCheck[b] &&
-        boardToCheck[a] === boardToCheck[c]
-      ) {
-        return boardToCheck[a];
-      }
+  // revisamos todas las combinaciones ganadoras
+  for (const combo of WINNER_COMBOS) {
+    const [a, b, c] = combo;
+    if (
+      boardToCheck[a] &&
+      boardToCheck[a] === boardToCheck[b] &&
+      boardToCheck[a] === boardToCheck[c]
+    ) {
+      return boardToCheck[a];
     }
-    // no hay ganador
-    return null;
   }
+  // no hay ganador
+  return null;
+}
 
-  
-
-  export const checkEndGame = (newBoard) => {
-    return newBoard.every((square) => square != null);
-  }
+export const checkEndGame = (newBoard) => {
+  return newBoard.every((square) => square != null);
+}
